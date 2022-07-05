ad0e72f83f8f6ba4ab7f1c9f1ad0423216395dd8
edricgalentino <erdrikpertama@gmail.com>
2022-07-05 14:37:50 +0700

    payment

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const productRoutes = require("./routes/product");
 const orderRoutes = require("./routes/order");
 const cartRoutes = require("./routes/cart");
+const paymentRoutes = require("./routes/stripe");
 
 dotenv.config();
 
@@ -19,12 +21,14 @@ mongoose
         console.log(`Error connecting to MongoDB: ${err}`);
     });
 
+app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 app.use("/product", productRoutes);
 app.use("/order", orderRoutes);
 app.use("/cart", cartRoutes);
+app.use("/payment", paymentRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server is running on port 5000...");
