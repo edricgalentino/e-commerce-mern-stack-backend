e6249505f956da28be1cc14cec67be30667ade2e
edricgalentino <erdrikpertama@gmail.com>
2022-07-08 14:01:09 +0700

    change product schema

--- models/Product.js
+++ models/Product.js
@@ -7,8 +7,8 @@ const ProductSchema = new mongoose.Schema(
         img: { type: String, required: true },
         description: { type: String, required: true },
         category: { type: Array },
-        size: { type: String },
-        color: { type: String },
+        size: { type: Array },
+        color: { type: Array },
     },
     { timestamps: true }
 );
