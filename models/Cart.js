6476eb9a91185289864a35b6a39e8a10202e49ba
edricgalentino <erdrikpertama@gmail.com>
2022-07-18 09:26:21 +0700

    update schema cart

--- models/Cart.js
+++ models/Cart.js
@@ -3,12 +3,17 @@ const mongoose = require("mongoose");
 const CartSchema = new mongoose.Schema(
     {
         userId: { type: String, required: true },
+        total: { type: Number, required: true },
+        quantity: { type: Number, required: true },
         products: [
             {
                 productId: { type: String },
+                name: { type: String },
                 quantity: { type: Number, default: 1 },
                 size: { type: String, default: "M" },
                 color: { type: String, default: "black" },
+                img: { type: String },
+                price: { type: String },
             },
         ],
     },
